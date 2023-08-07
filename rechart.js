4a877e3b8f29bdfecc94403664ae98454fd64187
productionbreaker <139469406+productionbreaker@users.noreply.github.com>
2023-08-07 12:19:44 +0530

    Update rechart.js

--- rechart.js
+++ rechart.js
@@ -1,3 +1,11 @@
 change color based on value 
 
  <Bar dataKey="value" fill={(data) => (data.value > 20 ? 'green' : 'red')} />
+
+
+
+ <Bar stackId="a" dataKey="premium" >
+        {data.map((entry, index) => (
+            <Cell fill={entry.years === "Current" ? '#290a0a' : '#005599' }/>
+        ))}
+    </Bar>
